8135fb5d328c38d06737845cb0cbe68665f6afb5
Daniel Gruno <humbedooh@apache.org>
2025-10-07 23:07:53 +0200

    OF: fix URI paths

--- app/endpoints/oauth.py
+++ app/endpoints/oauth.py
@@ -48,10 +48,10 @@ def make_client():
     provider = oic.oic.message.ProviderConfigurationResponse(
         version="1.0",
         issuer=config.oidc.issuer,
-        authorization_endpoint=config.oidc.endpoint + "auth",
+        authorization_endpoint=config.oidc.endpoint + "authorize",
         token_endpoint=config.oidc.endpoint + "token",
         jwks_uri=config.oidc.endpoint + "certs",
-        userinfo_endpoint=config.oidc.endpoint + "userinfo",
+        userinfo_endpoint=config.oidc.endpoint + "jwks",
     )
 
     client.handle_provider_config(provider, provider["issuer"])
